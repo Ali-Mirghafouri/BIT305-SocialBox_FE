0a9aad4ae412729e9456bdb49169099880028ee5
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2024-03-05 20:52:19 +0800

    logic done

--- src/app/pages/buyerHomePage/page.tsx
+++ src/app/pages/buyerHomePage/page.tsx
@@ -3,7 +3,9 @@
 import Image, { StaticImageData } from "next/image";
 import "./buyerHomePage.css";
 import { img } from "@/app/assets";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 interface CardProps {
   title: string;
@@ -17,7 +19,6 @@ interface FilterProps {
 }
 
 export default function BuyerHomePage() {
-  const [activeTab, setActiveTab] = useState(0);
   const assets = [
     {
       img: img.neon,
@@ -28,6 +29,132 @@ export default function BuyerHomePage() {
     {
       img: img.neon,
       title: "Neon Aliens",
+      author: "Creator 2",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 3",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 4",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 5",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 6",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 7",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 8",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 9",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 10",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 11",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 12",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 13",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 14",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 15",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 16",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 17",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 18",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
       author: "Creator 1",
       price: "0.9 ETH",
     },
@@ -61,57 +188,358 @@ export default function BuyerHomePage() {
       author: "Creator 1",
       price: "0.9 ETH",
     },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
-    // {
-    //   img: img.neon,
-    //   title: "Neon Aliens",
-    //   author: "Creator 1",
-    //   price: "0.9 ETH",
-    // },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
+    {
+      img: img.neon,
+      title: "Neon Aliens",
+      author: "Creator 1",
+      price: "0.9 ETH",
+    },
   ];
+
+  const [activeTab, setActiveTab] = useState(0);
+  const [assetSelector, setAssetSelector] = useState(0);
+  const [pageNum] = useState(Math.round(assets.length / 8));
+  const [activePage, setActivePage] = useState(1);
+  const [selectedAssets, setSelectedAssets] = useState<any>([...assets]);
 
   const filters = ["All", "Recent", "Low To High"];
 
   function Filter({ text, index }: FilterProps) {
-    console.log(index);
-
     return (
       <button
+        onClick={() => {
+          setActiveTab(index);
+        }}
         className={
           index === activeTab
             ? "buyerPage_filter_button_active"
@@ -147,6 +575,168 @@ export default function BuyerHomePage() {
     );
   }
 
+  function DisplayPagination() {
+    let num = [];
+
+    if (pageNum > 4) {
+      for (let index = 1; index <= pageNum; index++) {
+        num.push(index);
+      }
+
+      return (
+        <div className="flex">
+          <div
+            onClick={() => {
+              if (activePage !== 1) {
+                setActivePage(activePage - 1);
+                setAssetSelector(assetSelector - 8);
+              }
+            }}
+          >
+            <ArrowBackIosNewIcon />
+          </div>
+          {num.map((index) => {
+            if (index === 1) {
+              return (
+                <div
+                  className={
+                    index === activePage
+                      ? "buyerPage_page_num_active"
+                      : "buyerPage_page_num"
+                  }
+                >
+                  {index}
+                </div>
+              );
+            }
+            if (index < 4 && activePage < 4 && index !== 1) {
+              return (
+                <div
+                  className={
+                    index === activePage
+                      ? "buyerPage_page_num_active"
+                      : "buyerPage_page_num"
+                  }
+                >
+                  {index}
+                </div>
+              );
+            }
+
+            if (index !== pageNum && index === 4 && activePage < 4) {
+              return <div>...</div>;
+            }
+            if (
+              index !== pageNum &&
+              index < activePage + 2 &&
+              index > activePage - 2 &&
+              activePage > 3
+            ) {
+              return (
+                <div
+                  className={
+                    index === activePage
+                      ? "buyerPage_page_num_active"
+                      : "buyerPage_page_num"
+                  }
+                >
+                  {index}
+                </div>
+              );
+            }
+
+            if (
+              activePage > 3 &&
+              (index == activePage + 2 || index == activePage - 2) &&
+              index !== pageNum
+            ) {
+              return <div>...</div>;
+            }
+
+            if (index === pageNum) {
+              return (
+                <div
+                  className={
+                    index === activePage
+                      ? "buyerPage_page_num_active"
+                      : "buyerPage_page_num"
+                  }
+                >
+                  {index}
+                </div>
+              );
+            }
+          })}
+          <div
+            onClick={() => {
+              if (activePage !== pageNum) {
+                setActivePage(activePage + 1);
+                setAssetSelector(assetSelector + 8);
+              }
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </div>
+        </div>
+      );
+    } else {
+      for (let index = 1; index <= pageNum; index++) {
+        num.push(index);
+      }
+
+      return (
+        <div className="flex">
+          <div
+            onClick={() => {
+              if (activePage !== 1) {
+                setActivePage(activePage - 1);
+                setAssetSelector(assetSelector - 7);
+              }
+            }}
+          >
+            <ArrowBackIosNewIcon />
+          </div>
+          {num.map((index) => (
+            <div
+              className={
+                index === activePage
+                  ? "buyerPage_page_num_active"
+                  : "buyerPage_page_num"
+              }
+            >
+              {index}
+            </div>
+          ))}
+          <div
+            onClick={() => {
+              if (activePage !== pageNum) {
+                setActivePage(activePage + 1);
+                setAssetSelector(assetSelector + 7);
+              }
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </div>
+        </div>
+      );
+    }
+  }
+
+  useEffect(() => {
+    let tempAssets: any = [];
+    for (let index = assetSelector; index < assetSelector + 8; index++) {
+      assets[index] ? tempAssets.push(assets[index]) : undefined;
+    }
+    setSelectedAssets([...tempAssets]);
+  }, []);
+  useEffect(() => {
+    let tempAssets: any = [];
+    for (let index = assetSelector; index < assetSelector + 8; index++) {
+      assets[index] ? tempAssets.push(assets[index]) : undefined;
+    }
+    setSelectedAssets([...tempAssets]);
+  }, [activeTab, activePage]);
+
   return (
     <div className="buyerPage ">
       <div className="buyerPage_border">
@@ -157,14 +747,19 @@ export default function BuyerHomePage() {
           ))}
         </div>
         <div className="buyerPage_imgList">
-          {assets.map(({ author, img, price, title }) => (
+          {selectedAssets.map(({ author, img, price, title }: CardProps) => (
             <Card author={author} img={img} price={price} title={title} />
           ))}
         </div>
-        <div className="flex">
-          <div className="buyerPage_result">results</div>
+        <div className="flex buyerPage_bottom_container">
+          <div className="buyerPage_result">
+            {"Results " + (assetSelector + 1) + " - " + (assetSelector + 8)}
+            <span>{" out of " + assets.length}</span>
+          </div>
           <div className="flex_grow" />
-          <div className="buyerPage_page">pages</div>
+          <div className="buyerPage_page">
+            <DisplayPagination />
+          </div>
         </div>
       </div>
     </div>
